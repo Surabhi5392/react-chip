6ff4a77903a1f006f6a795e110f08ae008fdd556
Jain <surabhi.jain@hp.com>
2019-11-25 17:48:36 +0530

    initial commit

---
+++ site/src/Root.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import BasicExample from './basic';
+
+import CustomChipThemeExample from './customChipTheme';
+
+const Root = () => (
+  <div>
+    <h1>Chips with muli selection</h1>
+    <BasicExample />
+    
+  </div>
+);
+
+export default Root;
---
+++ site/src/basic.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Chips, { Chip } from '../../src'
+
+const data = [
+  'JavaScript',
+  'Ruby',
+  'Python',
+  'Java',
+  'Swift',
+  'C++',
+  'C',
+  'Objective C',
+  'Go'
+];
+
+class BasicExample extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: []
+    }
+  }
+
+  onChange = value => {
+    this.setState({ value });
+  }
+
+  render() {
+    return (
+      <Chips
+        style={{width: 60}}
+        value={this.state.value}
+        onChange={this.onChange}
+        createChipKeys={['a']}
+        placeholder="Type a Programming Language"
+        suggestions={data}
+        shouldRenderSuggestions={value => value.length >= 0}
+        fromSuggestionsOnly={false} />
+    );
+  }
+}
+
+export default BasicExample;
